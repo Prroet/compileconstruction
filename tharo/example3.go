cd25c9c328c46cdff71cdd946f6e75c25036bf47
Andreas Klöker <andreas@acerLaptop>
2016-04-25 20:18:30 +0200

    2 examples for go

---
+++ tharo/example3.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	if x==y { return 0, 0 }
+	return
+}
+
+// taken from go tour 2.4
+func main() {
+	fmt.Println(split(17))
+	x := 1
+	y := 0
+	if(y != x)
+		fmt.Println("y != x")
+}
+
