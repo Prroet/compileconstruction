69fdbdaffe000555a9a99c0ebc39591438c31ce8
prroet <prroet@gmail.com>
2016-05-18 10:13:20 +0200

    Removed all Object files

--- Jenny/ParserCC/build/Debug/GNU-Linux/TreeNode.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/GNU-Linux/TreeNode.o: TreeNode.cpp TreeNode.h \
- ../../Andreas/include/Lexer.h ../../Andreas/include/Token.h
-
-TreeNode.h:
-
-../../Andreas/include/Lexer.h:
-
-../../Andreas/include/Token.h:
